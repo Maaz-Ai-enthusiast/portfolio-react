b8ad9154f37b0aef6a1f46642b61724c83b641a9
Maaz-Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2024-09-10 22:06:45 +0500

    change logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,21 @@
 import "./navbar.css";
 import { useState } from "react";
-import { useRef } from "react";
 import logo1 from "./../../assets/images/logo1.gif";
 import menu_open from "./../../assets/images/menu_open.svg";
 import menu_close from "./../../assets/images/menu_close.svg";
 import underline from "./../../assets/images/nav_underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 export default function Navbar() {
+
+
   const [menu, setMenu] = useState("home");
-
-
-  const menuRef=useRef();
-
-   const openMenu=()=>{
-    menuRef.current.style.right="0"
-   }
-   const closeMenu=()=>{
-    menuRef.current.style.right="-350px"
-   }
 
   return (
     <div className="navbar">
       <img src={logo1} alt="logo" height={150}  className="nav-logo"/>
-      <img src={menu_open} onClick={openMenu}  alt="menu_icon" className="nav-mob-open" />
-      <ul ref={menuRef} className="nav-menu">
-        <img src={menu_close} onClick={closeMenu} alt="menu_icon" className="nav-mob-close" />
+      <img src={menu_open}   alt="menu_icon" className="nav-mob-open" />
+      <ul  className="nav-menu">
+        <img src={menu_close}  alt="menu_icon" className="nav-mob-close" />
         <li>
          <AnchorLink className="anchor-link"  href="#home"> <p onClick={() => setMenu("home")}>Home</p></AnchorLink>
           {menu === "home" ? <img src={underline} alt="img" /> : <></>}
